37a4ee8da33c1bf57eb7040dc7d322c298404678
daun Na <88607631+dawon0304@users.noreply.github.com>
2021-10-26 13:17:35 +0900

    엘라고 리디자인 coding

--- coding/index.js
+++ coding/index.js
@@ -9,3 +9,26 @@ $(document).ready(function () {
     arrows: true
   });
 });
+
+//Up Button
+$(document).ready(function () {
+  $(".bottomMenue>.topBtn>.btn>.fas").hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('.bottomMenue>.topBtn>.btn>.fas').fadeIn();
+    } else {
+      $('.bottomMenue>.topBtn>.btn>.fas').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('.bottomMenue>.topBtn>.btn>.fas').click(function () {
+    $('.bottomMenue>.topBtn>.btn>.fas').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+  $('.bottomMenue>.topBtn>.btn>.fas').tooltip('show');
+
+});
