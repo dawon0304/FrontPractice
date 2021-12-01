8b2c06d1a47d5b995f53992ae47e5d838a1cab52
daun Na <88607631+dawon0304@users.noreply.github.com>
2021-12-01 16:23:04 +0900

    event listen _ click event

--- JS Study/momentum/app.js
+++ JS Study/momentum/app.js
@@ -1,9 +1,22 @@
-const title = document.querySelector(".hello h1");
-console.log(title);
+const title = document.querySelector("div.hello:first-child h1");
+function handleTitleClick() {
+    console.log("title was clicked!");    
+}
 
-// 이는 hello란 class내부에 있는 h1을 가지고 올 수 있다는 것을 의미한다.
 
-// querySeloector에는 hello가 class name 이라는 것과 그안의 h1을 명시해줘야 하기 때문에  querySelctor ( ".hello h1" );  식으로 써줘야한다.
+title.addEventListener("click", handleTitleClick);
+// 여기서 클릭 이벤트를 리슨하고, 이 클릭 이벤트가 발생하면, handleTitleClick이라는 함수가 동작하길 원한다.
+// 이 handleTitleClick 함수를 실행하지 않게 하는것이 굉장히 중요하다.
+// 실행시키고 싶다면 7라인 위에 handleTitleClick()을 추가한다. 그치만 우리는 곧바로 실행시키고 싶지 않지
 
-//querySeloector는 첫번째로 검색된 elelment만 반환해준다.
-//만약 조건에 맞는 모든 element를 찾고싶다면, querySelectorAll을 사용하면된다.
+// 내가 원하는 것은, handleTitleClick 함수를 자바스크립트에 넘겨주고, 
+// 유저가 title을 click할 경우에 자바스크립트가 실행버튼을 대신 눌러주길 바라는 거다.
+
+
+// 즉, 내가 직접 실행 버튼을 누르지 않고, 자바스크립트에 handleTitleClick 함수 이름을 넘겨줘서, 유저가 타이틀을 클릭할 경우에, 자바스크립트가 나 대신 실행버튼을 눌러주게 하는것을 만들꺼다.
+
+// handleTitleClick() 특히 괄호!!를 추가함으로써 실행버튼을 누를 수 있다는 것 반드시 기억하기
+
+
+
+// addEventListener ("listen하고싶은 event이름", event가 발생하면 호출할 function);
