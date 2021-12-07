2cfb9e5323466b9d0dcb6e509937aa1823acbbda
daun Na <88607631+dawon0304@users.noreply.github.com>
2021-12-08 00:31:42 +0900

    Complete Momentum Web

--- JS Study/momentum/js/background.js
+++ JS Study/momentum/js/background.js
@@ -1,4 +1,4 @@
-const images = ["01.jpg", "02.jfif", "03.jpg", "04.jfif ", "05.jpg"];
+const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
 const bgImage = document.createElement("img");
